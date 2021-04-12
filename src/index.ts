23e0a814a6cd7008d903c3f7d30afed7536275e5
doritosxxx <doritosxxx@users.noreply.github.com>
2021-04-12 10:42:31 +0300

    add test

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import Caption from './Caption';
 import FractalTest from './Fractal/patterns/FractalTest'
 import CanvasProxy from './Proxy/CanvasProxy'
+import PlainProxy from './Proxy/PlainProxy';
 
-const fractal = new FractalTest(100, 100, 5)
 
-;(async() => {
+async function test1(){
+	const fractal = new FractalTest(500, 500, 100)
 	const caption = new Caption();
-	const proxy = new CanvasProxy(100, 100);
+	const proxy = new CanvasProxy(500, 500);
 	await fractal.generate(proxy, caption)
+	//console.log(proxy.graphics)
 	console.log(proxy.context.canvas.toDataURL())
-})()
+}
+
+
+test1()
