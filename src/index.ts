655171fa60009c423be80961d1ba904c194086be
doritosxxx <doritosxxx@users.noreply.github.com>
2021-04-12 12:42:17 +0300

    add test 2

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import Caption from './Caption';
+import FractalComplexFunction from './Fractal/patterns/FractalComplexFunction';
 import FractalTest from './Fractal/patterns/FractalTest'
 import CanvasProxy from './Proxy/CanvasProxy'
 import PlainProxy from './Proxy/PlainProxy';
 
-
+// CanvasProxy 500x500 and Test fractal.
+// Export to data uri.
 async function test1(){
-	const fractal = new FractalTest(500, 500, 100)
-	const caption = new Caption();
-	const proxy = new CanvasProxy(500, 500);
-	await fractal.generate(proxy, caption)
-	//console.log(proxy.graphics)
+	const fractal = new FractalTest(500, 500)
+	const proxy = new CanvasProxy(500, 500)
+	await fractal.generate(proxy)
 	console.log(proxy.context.canvas.toDataURL())
 }
 
+// CanvasProxy 500x500 and Complex function fractal.
+// Export to data uri.
+async function test2(){
+	const fractal = new FractalComplexFunction(500, 500)
+	const proxy = new CanvasProxy(500, 500)
+	await fractal.generate(proxy)
+	console.log(proxy.context.canvas.toDataURL())	
 
-test1()
+}
+
+
+test2()
