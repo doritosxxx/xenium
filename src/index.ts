6ce9740257346c8f6766cbc2d9fbce2a4843bd87
doritosxxx <doritosxxx@users.noreply.github.com>
2021-04-12 23:23:25 +0300

    add test 3,4

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
+import { Color } from './Drawing';
+import FractalComplexFunctionHole from './Fractal/patterns/FractalComplexFunction/FractalComplexFunctionHole';
+import FractalTest from './Fractal/patterns/FractalTest';
+import {CanvasProxy, PlainProxy} from './Proxy';
+import _fs  from 'fs'
+const fs = _fs.promises
+
+import path from 'path'
 import Caption from './Caption';
-import FractalComplexFunction from './Fractal/patterns/FractalComplexFunction';
-import FractalTest from './Fractal/patterns/FractalTest'
-import CanvasProxy from './Proxy/CanvasProxy'
-import PlainProxy from './Proxy/PlainProxy';
 
 // CanvasProxy 500x500 and Test fractal.
 // Export to data uri.
 async function test1(){
-	const fractal = new FractalTest(500, 500)
+	const fractal = new FractalTest(500, 500, 10, 
+		new Color("#333333"), 
+		new Color("#00FF88")
+	)
 	const proxy = new CanvasProxy(500, 500)
 	await fractal.generate(proxy)
 	console.log(proxy.context.canvas.toDataURL())
@@ -16,12 +23,44 @@ async function test1(){
 // CanvasProxy 500x500 and Complex function fractal.
 // Export to data uri.
 async function test2(){
-	const fractal = new FractalComplexFunction(500, 500)
+	const fractal = new FractalComplexFunctionHole(500, 500)
 	const proxy = new CanvasProxy(500, 500)
 	await fractal.generate(proxy)
-	console.log(proxy.context.canvas.toDataURL())	
+	const datauri = proxy.context.canvas.toDataURL()
+
+	fs.writeFile(path.resolve("./img.datauri"), datauri)
 
 }
 
+/**
+ * Color class test.
+ */
+function test3(){
+	const color1 = new Color(255, 0 , 117)
+	console.log(color1, color1+"")
 
-test2()
+	const color2 = Color.FromString("#FF0075")
+	console.log(color2, color2+"")
+	
+	const color3 = Color.FromString("#00FF88")
+	console.log(color3, color3+"")
+	
+	const color4 = new Color("#00FF88")
+	console.log(color4, color4+"")
+}
+
+/**
+ * Caption test Complex Function Fractal.
+ */
+async function test4(){
+	const fractal = new FractalComplexFunctionHole(100, 100)
+	const proxy = new PlainProxy()
+	const caption = new Caption()
+	await fractal.generate(proxy, caption)
+
+	console.log(caption.toString())
+
+
+}
+
+test4()
