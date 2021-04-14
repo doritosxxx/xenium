4fc8fb45ce778346989df9a43aafa157fe8b3535
doritosxxx <doritosxxx@users.noreply.github.com>
2021-04-14 10:05:30 +0300

    Update index.ts

--- src/Proxy/index.ts
+++ src/Proxy/index.ts
@@ -1,5 +1,6 @@
 import CanvasProxy from './CanvasProxy';
 import StorageProxy from './StorageProxy';
 import ProxyBase from './ProxyBase';
+import StubProxy from './StubProxy'
 
-export { ProxyBase, CanvasProxy, StorageProxy };
+export { ProxyBase, CanvasProxy, StorageProxy, StubProxy };
