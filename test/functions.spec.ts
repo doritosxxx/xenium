9dc268e5076772a4e550f07b0c83f78fc4145254
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-06-07 21:07:02 +0300

    Create functions.spec.ts

---
+++ test/functions.spec.ts
@@ -0,0 +1,64 @@
+import { pm, prec } from "../src/functions";
+
+// `prec` function tests.
+test("should leave number as is", () => {
+    expect(prec(1)).toEqual("1");
+});
+
+test("should leave number as is", () => {
+    expect(prec(1.1)).toEqual("1.1");
+});
+
+test("should remove last zero", () => {
+    expect(prec(1.1)).toEqual("1.1");
+});
+
+test("should keep only two digits after dot", () => {
+    expect(prec(1.12345)).toEqual("1.12");
+});
+
+test("should keep only one digit after dot", () => {
+    expect(prec(1.10345)).toEqual("1.1");
+});
+
+test("should return 0", () => {
+    expect(prec(0.0001234)).toEqual("0");
+});
+
+test("should return -0", () => {
+    expect(prec(-0.0001234)).toEqual("-0");
+});
+
+test("should keep only two digits after dot and also keep sign", () => {
+    expect(prec(-123.123)).toEqual("-123.12");
+});
+
+// `pm` function tests.
+test("should leave string as is", () => {
+    expect(pm("-123")).toBe("-123");
+});
+
+test("should append `+` at the beginning", () => {
+    expect(pm("123")).toBe("+123");
+});
+
+test("should append `+` at the beginning", () => {
+    expect(pm("0")).toBe("+0");
+});
+
+test("should leave number as is", () => {
+    expect(pm(-123)).toBe("-123");
+});
+
+test("should append `+` at the beginning", () => {
+    expect(pm(123)).toBe("+123");
+});
+
+test("should append `+` at the beginning", () => {
+    expect(pm(0)).toBe("+0");
+});
+
+test("should append `+` at the beginning", () => {
+	// That's wierd but `-0` converted to string is `0` (without minus sign).
+    expect(pm(-0)).toBe("+0");
+});
