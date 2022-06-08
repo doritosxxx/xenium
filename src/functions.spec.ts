4b60790758dbc69d876edf0a0da311e9a2295666
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-06-08 12:30:41 +0300

    Update functions.spec.ts

--- src/functions.spec.ts
+++ src/functions.spec.ts
@@ -2,35 +2,35 @@ import { pm, prec } from "./functions";
 
 // `prec` function tests.
 test("should leave number as is", () => {
-    expect(prec(1)).toEqual("1");
+    expect(prec(1)).toBe("1");
 });
 
 test("should leave number as is", () => {
-    expect(prec(1.1)).toEqual("1.1");
+    expect(prec(1.1)).toBe("1.1");
 });
 
 test("should remove last zero", () => {
-    expect(prec(1.1)).toEqual("1.1");
+    expect(prec(1.1)).toBe("1.1");
 });
 
 test("should keep only two digits after dot", () => {
-    expect(prec(1.12345)).toEqual("1.12");
+    expect(prec(1.12345)).toBe("1.12");
 });
 
 test("should keep only one digit after dot", () => {
-    expect(prec(1.10345)).toEqual("1.1");
+    expect(prec(1.10345)).toBe("1.1");
 });
 
 test("should return 0", () => {
-    expect(prec(0.0001234)).toEqual("0");
+    expect(prec(0.0001234)).toBe("0");
 });
 
 test("should return -0", () => {
-    expect(prec(-0.0001234)).toEqual("-0");
+    expect(prec(-0.0001234)).toBe("-0");
 });
 
 test("should keep only two digits after dot and also keep sign", () => {
-    expect(prec(-123.123)).toEqual("-123.12");
+    expect(prec(-123.123)).toBe("-123.12");
 });
 
 // `pm` function tests.
